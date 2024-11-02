398b149d1e57c6e59a9c95dbf4aa03ef56a39a1d
RomainGuirous <romaing_59@hotmail.fr>
2024-11-02 17:59:26 +0100

    Symfony09

--- migrations/Version20241102153425.php
+++ migrations/Version20241102153425.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241102145229 extends AbstractMigration
+final class Version20241102153425 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -4,16 +4,57 @@ namespace App\DataFixtures;
 
 use App\Entity\Program;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class ProgramFixtures extends Fixture
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
-    public function load(ObjectManager $manager): void
+    const PROGRAMS = [
+        [
+            'title' => 'Titre 1',
+            'synopsis' => 'some texte',
+            'category' => 'category_Action',
+        ],
+        [
+            'title' => 'Titre 2',
+            'synopsis' => 'some texte',
+            'category' => 'category_Aventure',
+        ],
+        [
+            'title' => 'Titre 3',
+            'synopsis' => 'some texte',
+            'category' => 'category_Animation',
+        ],
+        [
+            'title' => 'Titre 4',
+            'synopsis' => 'some texte',
+            'category' => 'category_Fantastique',
+        ],
+        [
+            'title' => 'Titre 5',
+            'synopsis' => 'some texte',
+            'category' => 'category_Horreur',
+        ]
+
+    ];
+
+    public function load(ObjectManager $manager)
     {
-        $product = new Program();
-        $product ->setTitle('Un titre');
-        $product ->setSynopsis('Un synopsis');
-        $manager->persist($product);
+        foreach(self::PROGRAMS as $programName)  {  
+            $program = new Program();
+            $program->setTitle($programName['title']);
+            $program->setSynopsis($programName['synopsis']);
+            $program->setCategory($this->getReference($programName['category']));
+            $manager->persist($program);
+        }
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        // Tu retournes ici toutes les classes de fixtures dont ProgramFixtures dépend
+        return [
+            CategoryFixtures::class,
+        ];
     }
 }
