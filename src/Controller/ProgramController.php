22ad97dbf12050db850d6281d721579e0985b37b
RomainGuirous <romaing_59@hotmail.fr>
2024-11-07 14:19:12 +0100

    Symfony13

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,9 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Entity\Program;
+use App\Entity\Season;
+use App\Entity\Episode;
 use App\Repository\EpisodeRepository;
 use App\Repository\SeasonRepository;
 use App\Repository\ProgramRepository;
@@ -25,22 +28,16 @@ class ProgramController extends AbstractController
     }
 
     //affichage d'une série, avec ses infos et ses saisons
-    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
-    public function show(int $id, ProgramRepository $programRepository, SeasonRepository $seasonRepository): Response
+    #[Route('/{id}', methods: ['GET'], name: 'show')]
+    public function show(int $id, Program $program, SeasonRepository $seasonRepository): Response
     {
         //obtient un programme selon son id
-        $program = $programRepository->findOneById($id);
+        // $program = $programRepository->findOneById($id);
         // $program = $programRepository->findOneBy(['id' => $id]);
         // $program = $programRepository->find($id);
 
-        // si ne trouve pas, crée erreur 404, sinon obtient les saison d'une série
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'Aucune série avec l\'id : ' . $id . ' n\'a été trouvée.'
-            );
-        } else {
+        //obtient les saison d'une série
             $seasons = $seasonRepository->findByProgram($id);
-        }
 
         return $this->render('program/show.html.twig', [
             'program' => $program,
@@ -49,43 +46,28 @@ class ProgramController extends AbstractController
     }
 
     //affichage d'une saison, ses infos et ses épisodes
-    #[Route('/{programId}/season/{seasonId}', requirements: ['programId' => '\d+', 'seasonId' => '\d+'], methods: ['GET'], name: 'season_show')]
-    public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository, EpisodeRepository $episodeRepository): Response
+    #[Route('/{program}/season/{season}', methods: ['GET'], name: 'season_show')]
+    public function showSeason(Program $program, Season $season, EpisodeRepository $episodeRepository): Response
     {
-        // $program = $programRepository->findOneBy(['id' => $id]);
-        // $program = $programRepository->find($id);
-        $program = $programRepository->findOneById($programId);
-        $seasonNumber = $seasonRepository->findOneByNumber($seasonId);
-
-        // les 2 premiers if vérifie que les id dans l'url existent, et si c'est le cas, le travail peut commencer
-        if (!$program) {
-            throw $this->createNotFoundException(
-                'Aucune série avec l\'id : ' . $programId . ' n\'a été trouvée.'
-            );
-        } elseif (!$seasonNumber) {
-            throw $this->createNotFoundException(
-                'Aucune saison ' . $seasonId . ' n\'a été trouvée.'
-            );
-        } else {
-            $seasonProgram = $seasonRepository->findByProgram($programId);
-
-            //obtention de la bonne saison de la bonne série -> $season
-            foreach ($seasonProgram as $seasonSingle) {
-                if ($seasonSingle->getNumber() == $seasonId) {
-                    $season = $seasonSingle;
-                }
-            }
-
-            //obtention des épisodes
-            $episodes = $episodeRepository->findBySeason($season->getId());
-            //obtention numero de la saison
-            $seasonNumber = $season->getNumber();
-        };
+        //obtention des épisodes
+        $episodes = $episodeRepository->findBySeason($season->getId());
 
         return $this->render('program/season_show.html.twig', [
             'program' => $program,
             'episodes' => $episodes,
-            'season' => $seasonNumber
+            'season' => $season
         ]);
     }
+
+     //affichage d'un épisode avec ses infos
+     #[Route('/{program}/season/{season}/episode{episode}', methods: ['GET'], name: 'episode_show')]
+     public function showEpisode(Program $program, Season $season, Episode $episode): Response
+     {
+ 
+         return $this->render('program/episode_show.html.twig', [
+             'program' => $program,
+             'episode' => $episode,
+             'season' => $season
+         ]);
+     }
 }
